93a4f2632785c2ac6628be26aff4a8ded6cce41d
Jiasheng Lee <ljsalm089@gmail.com>
2022-08-19 14:20:11 +0800

    feat: add question: longest substring without repeating characters

---
+++ longest_substring_without_repeating.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+"""
+Given a string, find the length of the longest substring without repeating
+characters.
+Can you find a solution in linear time?
+"""
+
+__author__ = 'Jiasheng Lee'
+
+
+class Solution(object):
+
+    def lengthOfLongestSubstring(self, s):
+        pass
+
+print Solution().lenthOfLongestSubstring('asdfghjkkuyttravxccb')
